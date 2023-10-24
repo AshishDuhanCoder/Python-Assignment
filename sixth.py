8fa99bbb182e8d54fe48697c5c226b5e4c8f5305
Ashish Duhan <51470938+AshishDuhanCoder@users.noreply.github.com>
2023-10-24 08:07:26 +0530

    Add files via upload

---
+++ sixth.py
@@ -0,0 +1,8 @@
+list = []
+for i in range(1,6):
+    a=int(input("Enter Number: "))
+    list.append(a)
+print(list)
+a=int(input("Enter the position: "))
+list.pop(2)
+print(list)
---
+++ third.py
@@ -0,0 +1,6 @@
+list=[]
+for i in range(1,6):
+    list.append(i)
+print(list)
+list.extend([6,7,8])
+print(list)
