694b85b114da13c82afe7f0d1fb96213070078aa
Ashish Duhan <51470938+AshishDuhanCoder@users.noreply.github.com>
2023-10-27 08:57:41 +0530

    Add files via upload

--- Que1.py
+++ Que1.py
@@ -1,3 +1,8 @@
-a = int(input("Enter a Number: "))
+myset = set({})
 for i in range(1,11):
-    print(a*i)
+    a=input("Acceptn 10 values: ")
+    myset.add(a)
+print(myset)
+a=input("Accept one more values: ")
+myset.update(a)
+print(myset)
